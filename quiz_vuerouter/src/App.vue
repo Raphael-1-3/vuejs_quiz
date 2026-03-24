03b847e83e9b45e572ee1a80f239731b5f578cf6
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2026-03-24 16:24:07 +0100

    init page du client pour realiser des quizs

--- quiz_vuerouter/src/App.vue
+++ quiz_vuerouter/src/App.vue
@@ -7,6 +7,8 @@
     <router-link to="/about">About</router-link>
     |
     <router-link to="/questionnaire">Qestionnaire</router-link>
+    |
+    <router-link to="/client">Jouer</router-link>
   </nav>
 
   <router-view />
--- quiz_vuerouter/src/js/router.js
+++ quiz_vuerouter/src/js/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/about.vue'
 import questionnaire from '@/views/questionnaire.vue'
+import client from '@/views/client.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
   { path: '/questionnaire', component: questionnaire },
+  { path: '/client', component: client },
   
 ]
 
